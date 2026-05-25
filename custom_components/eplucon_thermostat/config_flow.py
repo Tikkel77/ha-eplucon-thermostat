0d53a2ee639dda576b6214ec984a9a5dfda754bd
Bart Groosman <bart@groosman.nl>
2026-05-25 16:31:18 +0200

    Fix OptionsFlow init signature

--- custom_components/eplucon_thermostat/config_flow.py
+++ custom_components/eplucon_thermostat/config_flow.py
@@ -132,15 +132,11 @@ class EpluconThermostatConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         config_entry: config_entries.ConfigEntry,
     ) -> config_entries.OptionsFlow:
         """Create the options flow."""
-        return EpluconOptionsFlowHandler(config_entry)
+        return EpluconOptionsFlowHandler()
 
 
 class EpluconOptionsFlowHandler(config_entries.OptionsFlow):
     """Handle options for Eplucon Thermostat."""
-
-    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
-        """Initialize options flow."""
-        self.config_entry = config_entry
 
     async def async_step_init(
         self, user_input: dict[str, Any] | None = None
